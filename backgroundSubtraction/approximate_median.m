0432b744bcc95b563ead59aad75a310ef5e6be12
Roger Xu <rogerxu@ugrad16.cs.jhu.edu>
2011-11-12 17:03:46 -0500

    the file now generates a video

--- backgroundSubtraction/approximate_median.m
+++ backgroundSubtraction/approximate_median.m
@@ -6,31 +6,12 @@ clear all
 
 %source = mmreader('../test_video/traffic.avi');
 %source = mmreader('../test_video/san_fran_traffic_30sec_QVGA_Cinepak.avi');
-% source = aviread('C:\Video\Source\traffic\san_fran_traffic_30sec_QVGA');
-
-% source = [];
-% 
-% for i = 0000:100
-%     S = '../traffic-images/traffic-';
-%     if (i < 10)
-%         S = strcat(S, '000', num2str(i));
-%     elseif (i < 100)
-%         S = strcat(S, '00', num2str(i));
-%     elseif (i < 1000)
-%         S = strcat(S, '0', num2str(i));
-%     else
-%         S = strcat(S, num2str(i));
-%     end
-%     S = strcat(S, '.jpeg');
-%     I = imread(S);
-%     source = [source; I];
-% end
+%source = aviread('C:\Video\Source\traffic\san_fran_traffic_30sec_QVGA');
 
 thresh = 25;           
 
 %bg = source(1).cdata;           % read in 1st frame as background frame
 bg = imread('../traffic-images/traffic-0000.jpeg');
-imshow(bg);
 bg_bw = double(rgb2gray(bg));     % convert background to greyscale
 
 % ----------------------- set frame size variables -----------------------
@@ -41,11 +22,20 @@ fg = zeros(height, width);
 
 % --------------------- process frames -----------------------------------
 
-%for i = 2:length(source)
 
-for i = 2:3
-    %fr = source(i).cdata; 
-    S = strcat('../traffic-images/traffic-000',num2str(i),'.jpeg');
+for i = 1:1098
+    S = '../traffic-images/traffic-';
+    if (i < 10)
+        S = strcat(S, '000', num2str(i));
+    elseif (i < 100)
+        S = strcat(S, '00', num2str(i));
+    elseif (i < 1000)
+        S = strcat(S, '0', num2str(i));
+    else
+        S = strcat(S, num2str(i));
+    end
+    S = strcat(S, '.jpeg');
+    
     fr = imread(S);
     fr_bw = rgb2gray(fr);       % convert frame to grayscale
     
@@ -69,14 +59,14 @@ for i = 2:3
          end    
     end
 
-    figure(1),subplot(3,1,1),imshow(fr)     
-    subplot(3,1,2),imshow(uint8(bg_bw))
-    subplot(3,1,3),imshow(uint8(fg))   
+    %figure(1),   
+    %subplot(2,1,1),imshow(uint8(bg_bw))
+    %subplot(2,1,2),imshow(uint8(fg))   
     
-%     M(i-1)  = im2frame(uint8(fg),gray);             % save output as movie
-%     M(i-1)  = im2frame(uint8(bg_bw),gray);             % save output as movie
+    M(i) = im2frame(uint8(fg),gray(256));           % save output as movie
+    %M(i) = im2frame(uint8(bg_bw),gray);             % save output as movie
 end
 
-% movie2avi(M,'approximate_median_background','fps',30);           % save movie as avi    
+movie2avi(M,'approximate_median_background','fps',30);           % save movie as avi    
 
     
